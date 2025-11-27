51ca0c950a1aa42cbf8077fc34e9d5f18c810a07
pf_miles <miles.wy.1@gmail.com>
2025-11-28 01:08:55 +0800

    minor mod

--- battle_event_queue.lua
+++ battle_event_queue.lua
@@ -293,7 +293,7 @@ function macroTorch.computeLandTable(spell)
     local lastFail = macroTorch.peekFailEvent(spell)
     local lastFailedTime = lastFail and lastFail[1] or 0
     -- if no fail event near around the cast event, then it's a successful landed cast
-    if math.abs(lastFailedTime - lastCast) > 0.2 then
+    if math.abs(lastFailedTime - lastCast) > 0.05 then
         macroTorch.loginContext.landTable[spell][mob].push(lastCast)
         macroTorch.show(spell ..
             ' cast on ' ..
